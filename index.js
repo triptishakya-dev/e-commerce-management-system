dbabbc24400e0387cb27743c0c3e9aaf1af177b0
triptishakya-dev <triptishakya.2001@gmail.com>
2025-12-01 01:43:28 +0530

    feat: add CORS middleware to enable cross-origin requests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import addressRoutes from "./routers/addressRoutes.js";
 import orderRoutes from "./routers/orderRoutes.js";
 import productRoutes from "./routers/productRoutes.js";
@@ -6,6 +7,12 @@ import productRoutes from "./routers/productRoutes.js";
 const app = express()
 
 const  port = 3000
+
+app.use(cors({
+    origin: "http://localhost:3001", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
 
 app.get("/" ,(req,res) =>{
     res.send("hello word")
